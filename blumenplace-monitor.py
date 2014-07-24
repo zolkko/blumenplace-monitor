daeb2b2ccadf44e18ad5aea839c3dd33b17ad29a
zolkko <zolkko@gmail.com>
2014-07-24 14:09:43 +0400

    the application can read configuration from a configuration-file

--- blumenplace-monitor.py
+++ blumenplace-monitor.py
@@ -1,10 +1,59 @@
 # -*- coding: utf-8 -*-
 
+import sys
 
+from argparse import ArgumentParser, FileType
+from logging import getLogger, basicConfig, BASIC_FORMAT, DEBUG
+from logging.config import dictConfig as loggerDictConfig
+
+from anyjson import deserialize
+
+from monitor.app import MonitorApp
+
+
+def create_argument_parser():
+    parser = ArgumentParser(prog='blumenplace-monitor')
+    parser.add_argument('-c', '--config', action='store', type=FileType(mode='rt', encoding='utf-8'))
+    return parser
+
+
+def read_configuration(parser):
+    options = parser.parse_args(sys.argv[1:])
+
+    config_data = None
+
+    if options.config:
+        config_data = deserialize(options.config.read())
+        options.config.close()
+    else:
+        parser.print_help()
+
+    return config_data
+
+
+def init_loggers(logger_config):
+    if logger_config:
+        loggerDictConfig(logger_config)
+    else:
+        basicConfig(stream=sys.stdout, format=BASIC_FORMAT, level=DEBUG)
 
 
 def main():
-    pass
+    parser = create_argument_parser()
+    config = read_configuration(parser)
+
+    init_loggers(config.get('loggers', {}))
+
+    logger = getLogger(__name__)
+    logger.info('blumenplace-monitor application initialized.')
+
+    try:
+        app = MonitorApp(config)
+        app.run()
+    except Exception as ex:
+        logger.critical('blumenplace-monitor critical exception. Reason: {}.'.format(ex))
+
+    logger.info('blumenplace-monitor application finished.')
 
 
 if __name__ == '__main__':
--- monitor/app.py
+++ monitor/app.py
@@ -1,55 +1,24 @@
 # -*- coding: utf-8 -*-
 
-import sys
-import os
+from logging import getLogger
 
-from argparse import ArgumentParser
-
-from anyjson import deserialize
 from flask import Flask
 
 
-__all__ = ('start_app', )
+__all__ = ('MonitorApp', )
+
+
+logger = getLogger(__name__)
 
 
 class MonitorApp(Flask):
+
     APP_NAME = 'monitor'
 
-    def __init__(self, *kwargs):
-        """
-        static_path='', static_url_path='', static_folder='', template_folder='', instance_path=''
-
-        :param kwargs:
-        :return:
-        """
-        super(MonitorApp, self).__init__(MonitorApp.APP_NAME, **kwargs)
-
-
-def create_arg_parser():
-    parser = ArgumentParser()
-    parser.add_argument()
-    return parser
-
-
-def get_configuration():
-    parser = create_arg_parser()
-    args = parser.parse_args(sys.argv[1:])
-    if args.config and os.path.isfile(args.config):
-        with open(args.config, 'rt') as config_file:
-            configuration = deserialize(config_file.read())
-    else:
-        parser.print_help()
-
-
-def start_app():
-    parser = create_arg_parser()
-    args = parser.parse_args(sys.argv[1:])
-    if args.config and os.path.isfile(args.config):
-        with open(args.config, 'rt') as config_file:
-            configuration = deserialize(config_file.read())
-
-        app = MonitorApp()
-        app.config.from_object(configuration)
-        app.run()
-    else:
-        parser.print_help()
+    def __init__(self, config):
+        super(MonitorApp, self).__init__(
+            MonitorApp.APP_NAME,
+            static_url_path=config.get('staticUrl', None),
+            static_folder=config.get('staticFolder', 'static'),
+            template_folder=config.get('templateFolder', 'templates')
+        )
