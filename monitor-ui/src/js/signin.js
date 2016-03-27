a0fb7dd7d0f0b8eec37008134ea22b4367e0c2ff
zolkko <zolkko@gmail.com>
2016-03-27 03:43:03 +0300

    semantic ui css added

--- monitor-ui/src/js/signin.js
+++ monitor-ui/src/js/signin.js
@@ -1,13 +1,23 @@
-require("./../scss/signin.scss");
+import "semantic-ui/reset.css";
+import "semantic-ui/reset.css";
+import "semantic-ui/site.css";
+import "semantic-ui/container.css";
+import "semantic-ui/grid.css";
+import "semantic-ui/header.css";
+import "semantic-ui/form.css";
+import "semantic-ui/label.css";
+import "semantic-ui/input.css";
+import "semantic-ui/button.css";
+import "semantic-ui/icon.css";
+import "signin.scss";
 
-import App from "./signinapp";
-
+import App from "signinapp";
+import React from "react";
 
 var currentApp;
 
-
 document.addEventListener("DOMContentLoaded", () => {
-  if (!currentApp) {
-    currentApp = new App().initialize();
-  }
+    if (!currentApp) {
+        currentApp = new App().initialize();
+    }
 }, false);
--- monitor-ui/webpack.config.js
+++ monitor-ui/webpack.config.js
@@ -1,57 +1,80 @@
 "use strict";
 
 var path = require("path");
+var precss = require('precss');
+var autoprefixer = require('autoprefixer');
 var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: {
-    signin: ["babel-polyfill", "./src/js/signin.js"],
-    main: ["babel-polyfill", "./src/js/index.js"]
-  },
-  output: {
-    path: path.join(__dirname, "static"),
-    filename: "js/[name].js"
-  },
-  module: {
-    loaders: [
-      {
-        loader: "babel-loader",
-        test: /\.jsx?$/,
-        include: [
-          path.join(__dirname, "src/js")
+    entry: {
+        signin: ["./src/js/signin.js"],
+        vendor: ["react", "react-dom"]
+    },
+    output: {
+        path: path.join(__dirname, "static"),
+        filename: "js/[name].js"
+    },
+    module: {
+        loaders: [
+            {
+                loader: "babel-loader",
+                test: /\.jsx?$/,
+                include: [
+                    path.join(__dirname, "src/js")
+                ],
+                query: {
+                    plugins: ["transform-runtime"],
+                    presets: ["es2015", "stage-0", "react"]
+                }
+            },
+            {
+                loader: ExtractTextPlugin.extract("style", "!css!postcss"),
+                test: /\.css$/
+            },
+            {
+                loader: ExtractTextPlugin.extract("style", "!css!postcss!sass"),
+                test: /\.scss$/,
+                include: path.join(__dirname, "src/scss")
+            },
+            {
+                loader: 'file?name=[path][name].[ext]',
+                test: /\.(gif|svg|jpeg|png|jpg)$/
+            },
+            {
+                loader: 'file?name=[name].[ext]',
+                test: /\.(woff|woff2|ttf|eot)$/
+            }
+        ]
+    },
+    postcss: function () {
+        return [autoprefixer, precss];
+    },
+    resolve: {
+        root: [
+            path.join(__dirname, "src/js"),
+            path.join(__dirname, "src/scss")
         ],
-        query: {
-          plugins: ["transform-runtime"],
-          presets: ["es2015", "stage-0", "react"]
-        }
-      },
-      {
-        loader: ExtractTextPlugin.extract("style-loader", "!css-loader!sass-loader?sourceMap"),
-        test: /\.scss$/,
-        include: path.join(__dirname, "src/scss")
-      },
-      {
-        loader: ExtractTextPlugin.extract("style-loader", "!css-loader?sourceMap"),
-        test: /\.css$/,
-        include: path.join(__dirname, "src/css")
-      }
-    ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      _: "lodash"
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        screw_ie8: true,
-        warnings: false
-      }
-    }),
-    new ExtractTextPlugin("css/[name].css")
-  ],
-  taregt: "web",
-  debug: true,
-  devtool: "#inline-source-map"
+        alias: {
+            "semantic-ui": path.join(__dirname, "node_modules/semantic-ui-css/components")
+        },
+        modulesDirectories: ["node_modules"],
+        extensions: ["", ".js", ".jsx"]
+    },
+    resolveLoader: {
+        root: path.join(__dirname, "node_modules")
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin("vendor", "js/vendor.bundle.js"),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                screw_ie8: true,
+                warnings: false
+            }
+        }),
+        new ExtractTextPlugin("css/[name].css")
+    ],
+    taregt: "web",
+    debug: true,
+    devtool: "#inline-source-map"
 }
