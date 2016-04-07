e3149081dbe9bab843ebdafe23e718630382ba84
zolkko <zolkko@gmail.com>
2016-04-07 17:27:38 +0300

    Monitor UI. Disable state logging in production environment.

--- monitor-ui/src/js/store.js
+++ monitor-ui/src/js/store.js
@@ -3,13 +3,19 @@ import thunk from "redux-thunk";
 import monitorApp from "reducers";
 
 
-const logger = store => next => action => {
-    console.log('dispatching', action);
-    let result = next(action);
-    console.log('next state', store.getState());
-    return result;
-};
+debugger;
 
-let store = createStore(monitorApp, applyMiddleware(thunk, logger));
+let middleware = [thunk];
+
+if ("production" !== process.env.NODE_ENV) {
+    middleware.push(store => next => action => {
+        console.log('dispatching', action);
+        let result = next(action);
+        console.log('next state', store.getState());
+        return result;
+    });
+}
+
+let store = createStore(monitorApp, applyMiddleware(...middleware));
 
 export default store;
